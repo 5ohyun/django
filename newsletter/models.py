5a68819301d5bd006dec04572f025a0bbf7d5c94
5ohyun <66874455+5ohyun@users.noreply.github.com>
2020-09-18 16:58:39 +0900

    newsletter_modeling VIEWS.PY

---
+++ newsletter/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+# Create your models here.
+
+class News(models.Model) :
+    link = models.TextField()
+    title = models.TextField()
+    date = models.TextField()
+    content = models.TextField()
+    tag = models.TextField()
+    big_category = models.TextField()
+    category = models.TextField()
+    team_category = models.TextField()
+
+
+class New_News(models.Model) :
+    link = models.TextField()
+    tag = models.TextField()
+    content = models.TextField()
+    title = models.TextField()
+    date = models.TextField()
+    big_category = models.TextField()
